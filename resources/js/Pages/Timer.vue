14ce8538d6e98b0fab9f0a2ed2c99ff9dee46311
hueisu <aduxjj@gmail.com>
2023-02-13 21:47:52 +0800

    Add timer count function

--- resources/js/Pages/Timer.vue
+++ resources/js/Pages/Timer.vue
@@ -1,5 +1,19 @@
 <script setup>
 import { Head } from "@inertiajs/vue3";
+import { ref } from "vue";
+
+const time = ref(0);
+let intervalId;
+
+function startTimer() {
+    intervalId = setInterval(function () {
+        time.value++;
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(intervalId);
+}
 </script>
 
 <template>
@@ -10,10 +24,10 @@ import { Head } from "@inertiajs/vue3";
         <h1>Timer</h1>
         <div class="content">
             <div>Tag: Timer project</div>
-            <div id="time">00:00</div>
+            <div id="time">{{ time }}</div>
             <section class="timer-btn-container">
-                <button class="btn btn-blue">Start</button>
-                <button class="btn btn-red">Stop</button>
+                <button class="btn btn-blue" @click="startTimer">Start</button>
+                <button class="btn btn-red" @click="stopTimer">Stop</button>
             </section>
         </div>
     </div>
